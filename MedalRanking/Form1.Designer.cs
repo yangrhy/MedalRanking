dde8e7d66383acf534091d6b710679dd627f7af1
Ricky Yang <yangrhy@gmail.com>
2018-11-09 15:47:45 -0600

    Added NumUpDown to add picture box images of gold medals

---
+++ MedalRanking/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace MedalRanking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panelUSAbronze = new System.Windows.Forms.Panel();
+            this.numBronzeUSA = new System.Windows.Forms.NumericUpDown();
+            this.panelUSAsilver = new System.Windows.Forms.Panel();
+            this.numSilverUSA = new System.Windows.Forms.NumericUpDown();
+            this.panelUSAgold = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numGoldUSA = new System.Windows.Forms.NumericUpDown();
+            this.panelRUSbronze = new System.Windows.Forms.Panel();
+            this.numBronzeRUS = new System.Windows.Forms.NumericUpDown();
+            this.panelRUSsilver = new System.Windows.Forms.Panel();
+            this.numSilverRUS = new System.Windows.Forms.NumericUpDown();
+            this.panelRUSgold = new System.Windows.Forms.Panel();
+            this.numGoldRUS = new System.Windows.Forms.NumericUpDown();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panelUSAbronze.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numBronzeUSA)).BeginInit();
+            this.panelUSAsilver.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numSilverUSA)).BeginInit();
+            this.panelUSAgold.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numGoldUSA)).BeginInit();
+            this.panelRUSbronze.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numBronzeRUS)).BeginInit();
+            this.panelRUSsilver.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numSilverRUS)).BeginInit();
+            this.panelRUSgold.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numGoldRUS)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(-3, 1);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackgroundImage = global::MedalRanking.Properties.Resources.OlympicLogo;
+            this.splitContainer1.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.splitContainer1.Panel1.Controls.Add(this.panelUSAbronze);
+            this.splitContainer1.Panel1.Controls.Add(this.panelUSAsilver);
+            this.splitContainer1.Panel1.Controls.Add(this.panelUSAgold);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackgroundImage = global::MedalRanking.Properties.Resources.russia;
+            this.splitContainer1.Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.splitContainer1.Panel2.Controls.Add(this.panelRUSbronze);
+            this.splitContainer1.Panel2.Controls.Add(this.panelRUSsilver);
+            this.splitContainer1.Panel2.Controls.Add(this.panelRUSgold);
+            this.splitContainer1.Size = new System.Drawing.Size(605, 450);
+            this.splitContainer1.SplitterDistance = 236;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // panelUSAbronze
+            // 
+            this.panelUSAbronze.BackColor = System.Drawing.Color.Transparent;
+            this.panelUSAbronze.Controls.Add(this.numBronzeUSA);
+            this.panelUSAbronze.Location = new System.Drawing.Point(3, 163);
+            this.panelUSAbronze.Name = "panelUSAbronze";
+            this.panelUSAbronze.Size = new System.Drawing.Size(602, 69);
+            this.panelUSAbronze.TabIndex = 5;
+            // 
+            // numBronzeUSA
+            // 
+            this.numBronzeUSA.Location = new System.Drawing.Point(23, 17);
+            this.numBronzeUSA.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numBronzeUSA.Name = "numBronzeUSA";
+            this.numBronzeUSA.Size = new System.Drawing.Size(68, 20);
+            this.numBronzeUSA.TabIndex = 0;
+            this.numBronzeUSA.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // panelUSAsilver
+            // 
+            this.panelUSAsilver.BackColor = System.Drawing.Color.Transparent;
+            this.panelUSAsilver.Controls.Add(this.numSilverUSA);
+            this.panelUSAsilver.Location = new System.Drawing.Point(0, 80);
+            this.panelUSAsilver.Name = "panelUSAsilver";
+            this.panelUSAsilver.Size = new System.Drawing.Size(602, 69);
+            this.panelUSAsilver.TabIndex = 5;
+            // 
+            // numSilverUSA
+            // 
+            this.numSilverUSA.Location = new System.Drawing.Point(23, 17);
+            this.numSilverUSA.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numSilverUSA.Name = "numSilverUSA";
+            this.numSilverUSA.Size = new System.Drawing.Size(68, 20);
+            this.numSilverUSA.TabIndex = 0;
+            this.numSilverUSA.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // panelUSAgold
+            // 
+            this.panelUSAgold.BackColor = System.Drawing.Color.Transparent;
+            this.panelUSAgold.Controls.Add(this.button2);
+            this.panelUSAgold.Controls.Add(this.numGoldUSA);
+            this.panelUSAgold.Location = new System.Drawing.Point(3, 3);
+            this.panelUSAgold.Name = "panelUSAgold";
+            this.panelUSAgold.Size = new System.Drawing.Size(602, 69);
+            this.panelUSAgold.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gold;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(3, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Insert Gold Medals";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numGoldUSA
+            // 
+            this.numGoldUSA.Location = new System.Drawing.Point(23, 17);
+            this.numGoldUSA.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numGoldUSA.Name = "numGoldUSA";
+            this.numGoldUSA.Size = new System.Drawing.Size(68, 20);
+            this.numGoldUSA.TabIndex = 0;
+            this.numGoldUSA.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numGoldUSA.ValueChanged += new System.EventHandler(this.numGoldUSA_ValueChanged);
+            // 
+            // panelRUSbronze
+            // 
+            this.panelRUSbronze.BackColor = System.Drawing.Color.Transparent;
+            this.panelRUSbronze.Controls.Add(this.numBronzeRUS);
+            this.panelRUSbronze.Location = new System.Drawing.Point(3, 145);
+            this.panelRUSbronze.Name = "panelRUSbronze";
+            this.panelRUSbronze.Size = new System.Drawing.Size(602, 65);
+            this.panelRUSbronze.TabIndex = 5;
+            // 
+            // numBronzeRUS
+            // 
+            this.numBronzeRUS.Location = new System.Drawing.Point(23, 17);
+            this.numBronzeRUS.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numBronzeRUS.Name = "numBronzeRUS";
+            this.numBronzeRUS.Size = new System.Drawing.Size(68, 20);
+            this.numBronzeRUS.TabIndex = 0;
+            this.numBronzeRUS.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // panelRUSsilver
+            // 
+            this.panelRUSsilver.BackColor = System.Drawing.Color.Transparent;
+            this.panelRUSsilver.Controls.Add(this.numSilverRUS);
+            this.panelRUSsilver.Location = new System.Drawing.Point(0, 73);
+            this.panelRUSsilver.Name = "panelRUSsilver";
+            this.panelRUSsilver.Size = new System.Drawing.Size(602, 69);
+            this.panelRUSsilver.TabIndex = 5;
+            // 
+            // numSilverRUS
+            // 
+            this.numSilverRUS.Location = new System.Drawing.Point(23, 17);
+            this.numSilverRUS.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numSilverRUS.Name = "numSilverRUS";
+            this.numSilverRUS.Size = new System.Drawing.Size(68, 20);
+            this.numSilverRUS.TabIndex = 0;
+            this.numSilverRUS.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // panelRUSgold
+            // 
+            this.panelRUSgold.BackColor = System.Drawing.Color.Transparent;
+            this.panelRUSgold.Controls.Add(this.numGoldRUS);
+            this.panelRUSgold.Location = new System.Drawing.Point(3, 2);
+            this.panelRUSgold.Name = "panelRUSgold";
+            this.panelRUSgold.Size = new System.Drawing.Size(602, 69);
+            this.panelRUSgold.TabIndex = 3;
+            // 
+            // numGoldRUS
+            // 
+            this.numGoldRUS.Location = new System.Drawing.Point(23, 17);
+            this.numGoldRUS.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numGoldRUS.Name = "numGoldRUS";
+            this.numGoldRUS.Size = new System.Drawing.Size(68, 20);
+            this.numGoldRUS.TabIndex = 0;
+            this.numGoldRUS.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::MedalRanking.Properties.Resources.eight_col_olympic_continents_16x10;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Location = new System.Drawing.Point(602, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(262, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(99, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 89);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(94, 3);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 25);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 453);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Medal Ranking";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panelUSAbronze.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numBronzeUSA)).EndInit();
+            this.panelUSAsilver.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numSilverUSA)).EndInit();
+            this.panelUSAgold.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numGoldUSA)).EndInit();
+            this.panelRUSbronze.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numBronzeRUS)).EndInit();
+            this.panelRUSsilver.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numSilverRUS)).EndInit();
+            this.panelRUSgold.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numGoldRUS)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panelUSAbronze;
+        private System.Windows.Forms.NumericUpDown numBronzeUSA;
+        private System.Windows.Forms.Panel panelUSAsilver;
+        private System.Windows.Forms.NumericUpDown numSilverUSA;
+        private System.Windows.Forms.Panel panelUSAgold;
+        private System.Windows.Forms.NumericUpDown numGoldUSA;
+        private System.Windows.Forms.Panel panelRUSbronze;
+        private System.Windows.Forms.NumericUpDown numBronzeRUS;
+        private System.Windows.Forms.Panel panelRUSsilver;
+        private System.Windows.Forms.NumericUpDown numSilverRUS;
+        private System.Windows.Forms.Panel panelRUSgold;
+        private System.Windows.Forms.NumericUpDown numGoldRUS;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MedalRanking/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MedalRanking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int numGold = int.Parse(numGoldUSA.Value.ToString());
+            PictureBox pbGoldUSA = new PictureBox();
+            pbGoldUSA.Size = new Size(20, 20);
+            pbGoldUSA.BackgroundImage = Properties.Resources.gold;
+            pbGoldUSA.BackgroundImageLayout = ImageLayout.Stretch;
+            int x = 80; // for x coordinate
+            for (int i = 0; i < numGold; i++)
+            {
+                pbGoldUSA.Location = new Point(x + 20, 20);
+                panelUSAgold.Controls.Add(pbGoldUSA);
+                x += 20;
+            }
+        }
+
+        private void numGoldUSA_ValueChanged(object sender, EventArgs e)
+        {
+            int numGold = int.Parse(numGoldUSA.Value.ToString());
+            PictureBox pbGoldUSA = new PictureBox();
+            pbGoldUSA.Size = new Size(20, 20);
+            pbGoldUSA.BackgroundImage = Properties.Resources.gold;
+            pbGoldUSA.BackgroundImageLayout = ImageLayout.Stretch;
+            int x = 80; // for x coordinate
+            for (int i = 0; i < numGold; i++)
+            {
+                pbGoldUSA.Location = new Point(x + 20, 20);
+                panelUSAgold.Controls.Add(pbGoldUSA);
+                x += 20;
+            }
+        }
+    }
+}
